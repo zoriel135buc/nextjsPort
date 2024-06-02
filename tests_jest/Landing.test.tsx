352197c5ff663c30e7d1d9fac53c5673c5335391
zoriel135buc <zorielbuc135@gmail.com>
2024-06-02 12:59:16 +0300

    update

--- tests_jest/Landing.test.tsx
+++ tests_jest/Landing.test.tsx
@@ -1,27 +1,35 @@
 import "@testing-library/jest-dom";
-import { useInView } from "react-intersection-observer";
-import { render, screen, fireEvent } from "@testing-library/react";
-import { useIsMobile } from "@functional/useIsMobile";
+import { render, screen } from "@testing-library/react";
 import Landing from "@content/Landing";
 
-jest.mock("react-intersection-observer");
-
 describe("Landing", () => {
-  (useInView as jest.Mock).mockImplementation(() => [null, true]);
   test("renders the component", () => {
     render(<Landing />);
     const sectionElement = screen.getByTestId("Landing");
     expect(sectionElement).toBeInTheDocument();
-    expect(screen.getByTestId("Landing")).toBeInTheDocument();
   });
 
-  it("renders buttons with correct content", () => {
+  it("renders buttons with correct content", async () => {
     render(<Landing />);
 
-    const readExperienceButton = screen.getByText(/Read my Experience/i);
-    const readCVButton = screen.getByText(/Read my CV/i);
+    const readExperienceButton = screen.getByRole("button", {
+      name: /Latest work on GitHub/i,
+    });
+    const readCVButton = screen.getByRole("button", {
+      name: /Visit my LinkedIn profile/i,
+    });
 
     expect(readExperienceButton).toBeInTheDocument();
     expect(readCVButton).toBeInTheDocument();
+  });
+
+  it("should test the email button", async () => {
+    render(<Landing />);
+
+    const emailMeButton = screen.getByRole("link", { name: /Email Me/i });
+    expect(emailMeButton).toHaveAttribute(
+      "href",
+      "mailto:zorielbuc135@gmail.com"
+    );
   });
 });
